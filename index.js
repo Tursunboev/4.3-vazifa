33583cac1404d6c94854b955b0ee6b3a296cecbd
Tursunboev <akmalxontursunboyev52@gmaail.com>
2024-05-22 00:56:09 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,207 @@
+// 1-misol
+// let num1 = +prompt("son kiritng");
+// if (num1 > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 2-misol
+// let num1 = +prompt("son kiriting");
+// if (num1 % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 3-misol
+// let num1 = +prompt("son kiriting");
+// if (num1 % 2 == 0) {
+//   console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// 4-misol
+// let num1 = +prompt("son kiriting");
+// let num2 = +prompt("son kiriting");
+// if ((num1 > 2, num2 <= 3)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 5-misol
+// let num1 = +prompt("son kiriting");
+// let num2 = +prompt("son kiriting");
+// if ((num1 >= 0, num2 < -2)) {
+//   console.log(true);
+// }
+// else  {
+//     console.log(false);
+// }
+
+// 6-misol
+// let num1 = +prompt("son kiritng");
+// let num2 = +prompt("son kiritng");
+// let num3 = +prompt("son kiritng");
+// if (num1 <= num2 <= num3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 7-misol
+// let num1 = +prompt("son kiritng");
+// let num2 = +prompt("son kiritng");
+// let num3 = +prompt("son kiritng");
+// if (num1 <= num2 <= num3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 8 - misol
+// let num1 = +prompt("son kiritng");
+// let num2 = +prompt("son kiriting");
+// if (num1 % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// if (num2 % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 9-misol
+// let num1 = +prompt("son kiritng");
+// let num2 = +prompt("son kiriting");
+// if (num1 % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// if (num2 % 2 == 0) {
+//   console.log(num2);
+// } else {
+//   console.log(false);
+// }
+
+// 10-misol
+// let num1 = +prompt("son kiritng");
+// let num2 = +prompt("son kiriting");
+// if (num1 % 2 == 0) {
+//   console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+// if (num2 % 2 == 1) {
+//   console.log(num2);
+// }
+// else {
+//     console.log(false);
+// }
+
+// 11-misol
+// function areBothEvenOrOdd(A, B) {
+//   return A % 2 === B % 2;
+// }
+// // Example usage
+// let A = parseInt(prompt("Enter the first integer (A):"));
+// let B = parseInt(prompt("Enter the second integer (B):"));
+// let result = areBothEvenOrOdd(A, B);
+// console.log("The statement is", result);
+
+// 12-misol
+// let num1 = +prompt("son kiriting");
+// let num2 = +prompt("son kiriting");
+// let num3 = +prompt("son kiriting");
+// if (num1 > 0 && num2 > 0 && num3 > 0) {
+//   console.log(true);
+// // } else {
+//     console.log(false);
+// }
+// if (num1 > 0 && num2 > 0 && num3 > 0) {
+//   console.log(true);
+// }else {
+//     console.log(false);
+// }
+// // if (num1 > 0 && num2 > 0 && num3 > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 13-misol
+// function isAtLeastOnePositive(A, B, C) {
+//   return A > 0, B > 0, C > 0;
+// }
+// // Example usage
+// let A = parseInt(prompt("Enter the first integer (A):"));
+// let B = parseInt(prompt("Enter the second integer (B):"));
+// let C = parseInt(prompt("Enter the third integer (C):"));
+// let result = isAtLeastOnePositive(A, B, C);
+// console.log("The statement is", result);
+
+// 14-misol
+// function isAtOnlyOnePositive(A, B, C) {
+//   return A > 0, B < 0, C < 0;
+// }
+// // Example usage
+// let A = parseInt(prompt("Enter the first integer (A):"));
+// let B = parseInt(prompt("Enter the second integer (B):"));
+// let C = parseInt(prompt("Enter the third integer (C):"));
+// let result = isAtOnlyOnePositive(A, B, C);
+// console.log("The statement is", result);
+
+// 15-misol
+// function isAtLeastOneNegative(A, B, C) {
+//   return A > 0, B > 0, C < 0;
+// }
+// // Example usage
+// let A = parseInt(prompt("Enter the first integer (A):"));
+// let B = parseInt(prompt("Enter the second integer (B):"));
+// let C = parseInt(prompt("Enter the third integer (C):"));
+// let result = isAtLeastOneNegative(A, B, C);
+// console.log("The statement is", result);
+
+// 16-misol
+// let num1 = +prompt("A sonini kiriting");
+// if ((num1 > 9 && num1 < 100) % 2 == 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 17-misol
+// let num1 = +prompt("A sonini kiriting");
+// if ((num1 > 99 && num1 < 1000) % 2 == 0) {
+//   console.log(false);
+// } else {
+//   console.log(true);
+// }
+
+// 18-misol
+// let num1 = +prompt("sonni kiriting");
+// let num2 = +prompt("sonni kiriting");
+// let num3 = +prompt("sonni kiriting");
+
+// if ((num1 = num2 && num1 != num3)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// if ((num2 = num3 && num2 != num1)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// if ((num1 = num3 && num3 != num2)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
